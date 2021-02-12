5ce9cbc4c746f8122654b7a86717bdeb33295ad8
WishesFire <vmaksimus706@gmail.com>
2021-02-13 00:02:00 +0200

    initial commit

---
+++ core.py
@@ -0,0 +1,18 @@
+from telethon import TelegramClient
+from handlers import main
+import configparser
+
+# Get config telegram
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+
+if __name__ == '__main__':
+    api_tg = int(config['Telegram']['api_id'])
+    hash_tg = str(config['Telegram']['api_hash'])
+    username_tg = str(config['Telegram']['username'])
+
+    client = TelegramClient(username_tg, api_tg, hash_tg)
+    client.start()
+    with client:
+        client.loop.run_until_complete(main(client, username_tg))
---
+++ handlers.py
@@ -0,0 +1,20 @@
+async def main(client, username):
+    async for dialog in client.iter_dialogs():
+        print(dialog)
+        break
+
+
+def long_message():
+    pass
+
+
+def correspond_user():
+    pass
+
+
+def history_messages():
+    pass
+
+
+def online_message():
+    pass
